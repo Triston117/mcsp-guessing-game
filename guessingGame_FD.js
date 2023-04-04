887fb04a3f85ff8d2eb63d70db1193147459a1a4
Triston117 <roviiknaris@yahoo.com>
2023-04-04 11:16:11 -0700

    'Fixed numbers'

--- guessingGame_FD.js
+++ guessingGame_FD.js
@@ -1,4 +1,4 @@
-// 
+//
 // THIS WAS MY FIRST ROUGH DRAFT
 
 //let userNumber = prompt('Guess a number between 1-4!')
@@ -22,7 +22,7 @@
 // } else if (numberConversion <=3) {
 //     alert ("You've got to guess higher!")
 //      userLetter = prompt('Would you like to try again? Y || N')
-// } 
+// }
 // if (userLetter == 'Y') {
 //  userGuessAgain= prompt('Guess a number between 1-4!')
 //  guessCount++
@@ -40,11 +40,11 @@
 // }
 // // Need to make a would you like to play again function because i I'm getting tired of if else statements lol
 // function playAgain(){
-//     let playA = prompt('Would you like to play again Y or N') 
-//     if (playA === 'Y') {} 
+//     let playA = prompt('Would you like to play again Y or N')
+//     if (playA === 'Y') {}
 //     else if (playA === 'N') {}
-//     else 
-//     {alert('Invaild entry') 
+//     else
+//     {alert('Invaild entry')
 //     return playAgain}
 // }
 // // Need a guess count function?
@@ -99,11 +99,11 @@
 // guessNumber();
 // playAgain();
 
-// FINAL DRAFT 
+// FINAL DRAFT
 // Initialize empty object to store players' scores
 const players = {
-  'winston': [3,5,67],
-  'tyler':[1,3,17]
+  winston: [3, 5, 67],
+  tyler: [1, 3, 17],
 };
 
 // Function to play the game
@@ -134,7 +134,11 @@ function playGame() {
 
     // Check if the guess is correct
     if (guessNum === secretNumber) {
-      alert(`Congratulations ${name}! You guessed the number in ${numGuesses} guesses. Your guesses were: ${guesses.join(", ")}.`);
+      alert(
+        `Congratulations ${name}! You guessed the number in ${numGuesses} guesses. Your guesses were: ${guesses.join(
+          ", "
+        )}.`
+      );
 
       // Update the player's score if they played before
       if (players[name]) {
@@ -147,7 +151,7 @@ function playGame() {
       } else {
         players[name] = numGuesses;
       }
-      
+
       // Ask if the player wants to play again
       const playAgain = confirm("Do you want to play again?");
       if (playAgain) {
